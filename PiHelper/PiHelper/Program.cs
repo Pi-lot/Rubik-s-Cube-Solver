de5fbe3ec2756d4cc2975840caab5de007baa1ac
Bryce Tuton <brycey@tuton.org>
2019-06-10 00:29:21 +1000

    Fixed PiHelper. Now recieves, processes and sends back data.

--- PiHelper/PiHelper/Program.cs
+++ PiHelper/PiHelper/Program.cs
@@ -10,7 +10,6 @@ namespace PiHelper {
     class Program {
         public static void StartListening() {
             List<byte[]> data = new List<byte[]>();
-
             byte[] bytes = new byte[1024];
 
             IPHostEntry iPHostInfo = Dns.GetHostEntry(Dns.GetHostName());
@@ -36,38 +35,40 @@ namespace PiHelper {
                     data.Clear();
                     Console.WriteLine("Connection recieved from {0}", handler.RemoteEndPoint);
 
-                    int t = 0;
-                    int r = 0;
+                    int total = 0;
                     byte[] byteS = new byte[4];
                     do {
-                        r = handler.Receive(byteS);
+                        int r = handler.Receive(byteS);
                         Console.WriteLine("Revieved {0} bytes", r);
                         data.Add(byteS);
-                        t += r;
-                    } while (t < 4);
+                        total += r;
+                    } while (total < 4);
 
-                    byte[] s = new byte[4];
-                    if (data.Count > 1)
-                        for (int i = 0; i < data.Count; i++)
-                            for (int j = 0; j < data[i].Length; j++)
-                                s[i + j] = data[i][j];
-                    else
-                        s = data[0];
+                    List<byte> s = new List<byte>();
+                    foreach (byte[] bt in data)
+                        foreach (byte bb in bt)
+                            s.Add(bb);
                     data.Clear();
 
                     int loops = 0;
                     int count = 0;
-                    int size = BitConverter.ToInt32(s, 0);
+                    int size = BitConverter.ToInt32(s.ToArray(), 0);
                     Console.WriteLine("Expecting {0} bytes", size);
-                    int total = 0;
-                    do {
+                    total = 0;
+                    while (total < size) {
                         int recieved = handler.Receive(bytes);
-                        data.Add(bytes);
+                        byte[] rb = new byte[recieved];
+                        for (int i = 0; i < recieved; i++) {
+                            rb[i] = bytes[i];
+                        }
+                        data.Add(rb);
                         total += recieved;
                         if (recieved == bytes.Length)
                             count++;
                         loops++;
-                    } while (total < size);
+                    }
+                    data.Distinct();
+
                     Console.WriteLine("Buffer filled {0} times out of {1}", count, loops);
                     Console.WriteLine("Average bytes: {0}", total / loops);
                     Console.WriteLine("Percent: {0}", ((float)count / (float)loops) * 100);
@@ -75,77 +76,96 @@ namespace PiHelper {
                     Console.WriteLine("{0} bytes recieved", total);
                     Console.WriteLine("List size {0}", total / 54);
 
-                    Console.WriteLine("Sending back for clarification");
-                    foreach (byte[] piece in data)
-                        handler.Send(piece);
-
                     Console.Write("Reorganising data... ");
 
                     List<byte> b = new List<byte>();
-                    for (int i = 0; i < data.Count; i++)
-                        for (int j = 0; j < data[i].Length; j++)
-                            b.Add(data[i][j]);
+                    foreach (byte[] ba in data)
+                        foreach (byte bb in ba)
+                            b.Add(bb);
 
                     data.Clear();
 
-                    byte[] d = new byte[54];
-                    for (int i = 0; i < b.Count; i++) {
-                        d[i % 54] = b[i];
-                        if (i % 54 == 53)
-                            data.Add(d);
+                    for (int j = 0; j < b.Count; j += 54) {
+                        byte[] d = new byte[54];
+                        if (j + 54 > b.Count)
+                            d = new byte[b.Count - j];
+                        for (int i = 0; i < d.Length; i++) {
+                            d[i] = b[j + i];
+                        }
+                        data.Add(d);
                     }
+
+                    data.Distinct();
+
                     Console.WriteLine("Done");
 
-                    for (int i = 0; i < 4 * 54; i++) {
-                        Console.Write("|{0}|", b[i]);
-                        if (i % 54 == 53) {
-                            Console.WriteLine();
-
-                            foreach (byte pb in data[i % 54])
-                                Console.Write("|{0}|", pb);
-                            Console.WriteLine();
-                        }
-                    }
+                    Console.Write("Sending back for clarification... ");
+                    handler.Send(b.ToArray());
 
                     b.Clear();
 
+                    byte[] checkB = new byte[1];
+                    handler.Receive(checkB);
+
+                    Console.Write("Done. ");
+
+                    if (BitConverter.ToBoolean(checkB))
+                        Console.WriteLine("All Correct");
+                    else {
+                        Console.WriteLine("Data wrong");
+                        return;
+                    }
+
                     Console.Write("Removing Duplicates... ");
-                    byte[][] check = new byte[data.Count][];
-                    data.CopyTo(check);
-                    data.Clear();
-                    for (int i = 0; i < check.Length; i++) {
-                        if (data.Count < 1)
-                            data.Add(check[i]);
-                        else {
+                    List<byte[]> check = new List<byte[]>();
+                    for (int i = data.Count - 1; i >= 0; i--) {
+                        if (check.Count < 1) {
+                            check.Add(data[i]);
+                            data.RemoveAt(i);
+                        } else {
                             bool seen = false;
 
-                            for (int j = 0; j < data.Count; j++) {
-                                if (check[i].SequenceEqual(data[j])) {
+                            for (int j = 0; j < check.Count; j++) {
+                                if (data[i].SequenceEqual(check[j])) {
                                     seen = true;
                                     break;
                                 }
                             }
 
                             if (!seen)
-                                data.Add(check[i]);
+                                check.Add(data[i]);
+
+                            data.RemoveAt(i);
                         }
                     }
-                    check = new byte[0][];
+
+                    data.Clear();
+
+                    for (int i = check.Count - 1; i >= 0; i--) {
+                        data.Add(check[i]);
+                        check.RemoveAt(i);
+                    }
+
+                    check.Clear();
                     Console.WriteLine("Done. New length: {0}", data.Count);
 
                     Console.Write("Reorganising data... ");
                     foreach (byte[] ba in data)
                         foreach (byte bb in ba)
                             b.Add(bb);
-                    data.Clear();
+
                     Console.WriteLine("Done");
+                    data.Clear();
 
                     Console.Write("Sending data back... ");
+                    handler.Send(BitConverter.GetBytes(b.Count));
                     handler.Send(b.ToArray());
                     Console.WriteLine("Done");
+                    handler.Receive(bytes);
                     Console.WriteLine("Closing connection");
                     handler.Shutdown(SocketShutdown.Both);
                     handler.Close();
+                    handler.Dispose();
                 }
             } catch (Exception e) {
                 Console.WriteLine(e.ToString());
